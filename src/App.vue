0b2bd6deab2fd846de3ace0e9b969b780d0bb36d
CABeale <callum.beale.098@gmail.com>
2022-08-01 19:07:09 +1000

    pinia-course ~~~~~~~~
    Starting template updated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import ProductCard from './components/ProductCard.vue'
+import products from './data/products.json'
+</script>
 
 <template>
-	<h1 class="text-3xl font-bold underline">Hello World!</h1>
+	<div class="container">
+		<TheHeader />
+		<ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+			<ProductCard
+				v-for="product in products"
+				:key="product.name"
+				:product="product"
+			/>
+		</ul>
+	</div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './index.css'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import FontAwesomePlugin from './plugins/FontAwesome'
+import AppButton from './components/AppButton.vue'
+import AppCountInput from './components/AppCountInput.vue'
+import AppModalOverlay from './components/AppModalOverlay.vue'
+
+createApp(App)
+	.use(createPinia)
+	.use(FontAwesomePlugin)
+	.component('AppButton', AppButton)
+	.component('AppCountInput', AppCountInput)
+	.component('AppModalOverlay', AppModalOverlay)
+	.mount('#app')
